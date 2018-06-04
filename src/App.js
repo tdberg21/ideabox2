f0791730365ff6096bcb8a9dec8b8d9650d03697
Tory Dannenberg <rfdberg@gmail.com>
2018-06-04 10:58:50 -0600

    Add initial input fields and buttons as well as Idea component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Idea from './Idea.js'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.updateState = this.updateTitle.bind(this);
+
+    this.state = {
+      title: '',
+      toDos: []
+    };
+  };
+
+  updateTitle(event) {
+    this.setState({title: event.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to Ideabox</h1>
+          <p className="App-intro">
+          Tell me about your ideas...
+          </p>
+          <input type="text" placeholder="Idea Title" onChange={(event) => this.updateTitle(event)}></input>
+          <input type="text" placeholder="Idea Body"></input>
+          <button> Submit Idea </button>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Idea />
       </div>
     );
   }
---
+++ src/Idea.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Idea extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return(
+      <div>
+        <h2> Idea Title </h2>
+        <p> Idea Body goes here </p>
+      </div>
+    )
+  }
+}
